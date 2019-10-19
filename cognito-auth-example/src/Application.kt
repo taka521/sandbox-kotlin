e17a384c9bc0074764e46e66dbbc9dcb517b5c2f
taka521 <521taka.m@gmail.com>
2019-10-19 17:15:41 +0900

    Ktor + Cognitoによる認証サンプルプロジェクトを作成

---
+++ cognito-auth-example/src/Application.kt
@@ -0,0 +1,43 @@
+package com.taka521
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.features.*
+import org.slf4j.event.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/jackson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
